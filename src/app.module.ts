3de1a6eff0d7e63a86700d587b68d0d3b542be2c
mardinw <mardinw@outlook.com>
2024-01-27 09:25:02 +0700

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { CustomerController } from './customers/customer.controller';
 import { CustomerService } from './customers/customer.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env'],
-  }),
+  }), AuthModule, UsersModule,
   ],
   controllers: [AppController, CustomerController],
   providers: [CustomerService],
