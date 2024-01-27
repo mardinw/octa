851420b7ff2564ca96028a80a1d7fd85f99b8441
mardinw <mardinw@outlook.com>
2024-01-27 10:02:11 +0700

    initial commit

--- src/customers/customer.controller.ts
+++ src/customers/customer.controller.ts
@@ -5,34 +5,6 @@ import { Response } from 'express';
 @Controller('/accounts')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
-
-  @Get('master')
-  async getAllCustomers(@Query('page') page = 1, @Res() res: Response ) {
-    // Calculate the offset based on the page and limit
-    const limit = 10
-    const offset = (page - 1) * limit;
-    
-    const accounts = await this.customerService.query(
-      'SELECT bank, arco, product, card_number, first_name, last_name, sex, dob, job_title, current_balance, minimum_payment, credit_limit, charge, last_payment_amount, last_payment_date FROM customer LIMIT ? OFFSET ?',
-      [limit, offset]
-    );
-
-    // get total table in customer
-    const totalAccount = await this.customerService.query(
-      'SELECT COUNT(*) as count FROM customer'
-    );
-
-    const viewData = {
-      title: 'Master Account - OCTA',
-      subtitle: 'Master Account',
-      accounts,
-      page,
-      limit,
-      totalAccount: totalAccount[0].count,
-    };
-  
-   return res.render('accounts/master', { viewData: viewData })
-  }
   
   @Get('list')
   async getListCustomers(@Query('page') page = 1, @Res() res: Response ) {
--- src/helpers/numbers.helper.ts
+++ src/helpers/numbers.helper.ts
@@ -11,3 +11,10 @@ export function parseNumber(numberString) {
 export function formatNumber(number: number): string {
     return number.toLocaleString('id-ID', {style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2});
 }
+
+export function indexHelper(context, options) {
+    const startIndex = options.data.root.startIndex;
+    const index = startIndex + this.index;
+
+    return index;
+}
