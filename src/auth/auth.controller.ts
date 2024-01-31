6029d246142b5b4fc610d93c914aa3687eac2824
mardinw <mardinw@outlook.com>
2024-01-31 14:49:03 +0700

    initial commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Render, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('login')
+  @Render('auth/login')
+  login() {
+    const viewData = [];
+    viewData['title'] = 'User Login - OCTA';
+    viewData['subtitle'] = 'User Login';
+
+    return {viewData};
+  }
+  
+  @Get('register')
+  @Render('auth/register')
+  register() {
+    const viewData = [];
+    viewData['title'] = 'User Register - OCTA';
+    viewData['subtitle'] = 'User Register';
+
+    return {viewData};
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('connect')
+  connect(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
