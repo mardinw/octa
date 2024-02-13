36f3e1f6e6ad4b5016a8714edea7adf012428f71
mardinw <mardinw@outlook.com>
2024-02-13 09:52:44 +0700

    initial commit

---
+++ src/accounts/account.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Query, Render, Res, Session } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { Response } from 'express';
+
+@Controller('/accounts')
+export class AccountController {
+  constructor(private readonly customerService: AccountService) {}
+  
+  @Get('list')
+  @Render('accounts/list')
+  async findAll(@Query('page') page = '1', @Res() res: Response, @Session() session:Record<string, any> ) {
+
+    if (session.user) {
+      const currentPage : number = parseInt(page, 10);
+      const limit : number = 10
+      const offset : number = (currentPage - 1) * limit;
+
+      const customers = await this.customerService.customersWithLimitOffset(limit, offset);
+
+      const viewData = {
+        accounts: customers,
+        currentPage,
+        limit,
+      }
+
+      return { viewData };
+    } else {
+      return res.redirect('/auth/login');
+    }
+  }
+}
---
+++ src/accounts/account.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { initDatabaseConnection } from "../configs/db/mysql.utils";
+import { PrismaService } from "src/prisma/prisma.service";
+import { customer } from "@prisma/client";
+
+@Injectable()
+export class AccountService {
+
+  constructor(private prisma: PrismaService) {}
+
+  async customersWithLimitOffset(limit: number, offset: number): Promise<customer[]> {
+
+    const sanitizedLimit = Math.max(0, limit);
+    const sanitizedOffset = Math.max(0, offset);
+  
+    try {
+      const customers = await this.prisma.customer.findMany({
+        take: sanitizedLimit,
+        skip: sanitizedOffset,
+      })
+
+      const processedCustomers = customers.map((customer) => {
+        if (customer.ptp_date === null) {
+        }
+
+        return customer;
+      });
+
+      const filteredCustomers = processedCustomers.filter((customer) => customer !== null);
+
+      return filteredCustomers;
+    } catch (error) {
+        throw new Error(`Error fetching customers: ${error.message}`);
+    }
+  }
+}
